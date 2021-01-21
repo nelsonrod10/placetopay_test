bde6d366c4159c1dd893fed793dbe2fb09aa5c4f
Nelson Rodriguez <nelsonrod10@gmail.com>
2021-01-21 15:36:28 -0500

    feat: Add model, controller and migration to Products

---
+++ database/migrations/2021_01_21_202910_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->required();        
+            $table->string('price')->required();       
+            $table->string('currency')->required();    
+            $table->longText('description')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
