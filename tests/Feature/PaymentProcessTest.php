ec91ebd09a0a8f2627e5479ca84c1726c9114204
Nelson Rodriguez <nelsonrod10@gmail.com>
2021-01-22 16:30:48 -0500

    feat: Add PaymentProcessTest for test all the payment results

---
+++ tests/Feature/PaymentProcessTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Order;
+use App\Product;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PaymentProcessTest extends TestCase
+{
+    use RefreshDatabase;
+
+    
+
+    
+}
