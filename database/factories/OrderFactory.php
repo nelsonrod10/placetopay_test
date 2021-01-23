2a23b4132c043c2d311939572dfaa0d8ebcd75fb
Nelson Rodriguez <nelsonrod10@gmail.com>
2021-01-23 08:57:13 -0500

    feat: Edit OrderFactory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -2,11 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Order;
+use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Order::class, function (Faker $faker) {
     return [
-        //
+        'product_id'     => $faker->numberBetween(1,10),
+        'number'         =>$faker->numberBetween(100,2000),
+        'customer_name'  => $faker->name(),
+        'customer_email' => $faker->freeEmail,
+        'customer_mobile'=> $faker->phoneNumber,
+        'status'         => Arr::radom(['CREATED','PAYED','REJECTED']),
     ];
 });
