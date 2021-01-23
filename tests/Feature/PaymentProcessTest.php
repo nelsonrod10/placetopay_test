5942d133221df5a26948d37bf11bd67a65303ce8
Nelson Rodriguez <nelsonrod10@gmail.com>
2021-01-23 08:43:06 -0500

    create test for see the payment result

--- tests/Feature/PaymentProcessTest.php
+++ tests/Feature/PaymentProcessTest.php
@@ -41,7 +41,38 @@ class PaymentProcessTest extends TestCase
         $redirectResponse = $this->get('validate-payment/'.$newOrder->number);
 
         $redirectResponse->assertRedirect(route('payment-result',['reference' => $newOrder->number]));
-    }   
+    }
+    
+    /**
+     * A buyer can see the payment result.
+     * @test
+     * @return void
+     */
+    public function a_buyer_can_see_payment_result()
+    {
+        $this->withoutExceptionHandling();
+
+        factory(Product::class,1)->create();
+
+        $product = Product::first();
+
+        $data = [
+            'product_id'          => $product->id,
+            'customer_name'    => 'Nelson Rodriguez',
+            'customer_email'   => 'bejin3@hotmail.com',
+            'customer_mobile'  => '3167585671'  
+        ];
+
+        $response = $this->post('orders',$data);
+
+        $response->assertStatus(302);
+
+        $newOrder = $product->getOrder();
+        
+        $redirectResponse = $this->get('validate-payment/'.$newOrder->number);
+
+        $redirectResponse->assertRedirect(route('payment-result',['reference' => $newOrder->number]));
+    }
 
     
 }
