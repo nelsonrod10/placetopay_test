e44a6cda746e8bc98bab14de8b44afbd6ae5465f
Nelson Rodriguez <nelsonrod10@gmail.com>
2021-01-24 07:22:03 -0500

    fix: Add base layout to all of views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="flex items-center">
-        <div class="md:w-1/2 md:mx-auto">
+        <div class="w-full px-2 md:mx-auto">
 
             @if (session('status'))
                 <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
@@ -10,10 +10,10 @@
                 </div>
             @endif
 
-            <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+            <div class="flex flex-col break-words bg-white border-2 rounded shadow-md">
 
                 <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
-                    Dashboard
+                    Home page
                 </div>
 
                 <div class="w-full p-6">
---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex items-center">
+        <div class="w-full px-2 md:mx-auto">
+            <div class="flex flex-col break-words bg-white border-2 rounded shadow-md">
+
+                <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
+                    mostrar un producto
+                </div>
+
+                <div class="w-full p-6">
+                    <p class="text-gray-700">
+                        mostrar un producto
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
