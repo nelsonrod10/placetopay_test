9aa7890996df43a1dd4be0f19aea38172cffa537
Nelson Rodriguez <nelsonrod10@gmail.com>
2021-01-24 11:37:30 -0500

    feat: Create order store form in orders.create view

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -3,24 +3,52 @@
 @section('content')
     <div class="container mx-auto">
         <div class="flex flex-wrap justify-center">
-            <div class="w-full max-w-sm">
+            <div class="w-full md:w-2/4 px-2 py-4">
                 <div class="flex flex-col break-words bg-white border-2 rounded shadow-md">
 
                     <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
-                        {{ __('Login') }}
+                        Confirmación de compra
                     </div>
-
-                    <form class="w-full p-6" method="POST" action="{{ route('login') }}">
+                    <form class="w-full p-6" method="POST" action="{{ route('orders.store') }}">
                         @csrf
+                        <input type="hidden" class="hidden" name="product_id" value="{{$product->id}}">
+
+                        <div class="flex flex-col mb-6 text-gray-700">
+                            <div class="flex-1">
+                                <div class="font-semibold text-lg">Producto: </div> 
+                                <div class="overflow-hidden font-light text-xl mt-2">{{$product->name}}</div>
+                            </div>
+                            <div class="flex-1 mt-4">
+                                <div class="font-semibold text-lg">Precio total: </div> 
+                                <div class="overflow-hidden font-light text-xl mt-2">$ {{$product->price}} {{$product->currency}}</div>
+                            </div>
+                            <div class="flex-1 mt-6 text-center">
+                                <div class="font-semibold text-lg">Por favor diligencie la siguiente información </div> 
+                            </div>
+                        </div>
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">
+                                Nombre completo:
+                            </label>
+
+                            <input id="name" type="text" class="form-input w-full @error('customer_name') border-red-500 @enderror" name="customer_name" value="{{ old('customer_name') }}" autofocus>
+
+                            @error('customer_name')
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
 
                         <div class="flex flex-wrap mb-6">
                             <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
-                                {{ __('E-Mail Address') }}:
+                                Email:
                             </label>
 
-                            <input id="email" type="email" class="form-input w-full @error('email') border-red-500 @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" class="form-input w-full @error('customer_email') border-red-500 @enderror" name="customer_email" value="{{ old('customer_email') }}" autocomplete="email" autofocus>
 
-                            @error('email')
+                            @error('customer_email')
                                 <p class="text-red-500 text-xs italic mt-4">
                                     {{ $message }}
                                 </p>
@@ -28,45 +56,23 @@
                         </div>
 
                         <div class="flex flex-wrap mb-6">
-                            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
-                                {{ __('Password') }}:
+                            <label for="mobile" class="block text-gray-700 text-sm font-bold mb-2">
+                                Celular:
                             </label>
 
-                            <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror" name="password" required>
+                            <input id="mobile" type="number" maxlength="10" class="form-input w-full @error('customer_mobile') border-red-500 @enderror" name="customer_mobile" value="{{ old('customer_mobile') }}" >
 
-                            @error('password')
+                            @error('customer_mobile')
                                 <p class="text-red-500 text-xs italic mt-4">
                                     {{ $message }}
                                 </p>
                             @enderror
                         </div>
 
-                        <div class="flex mb-6">
-                            <label class="inline-flex items-center text-sm text-gray-700" for="remember">
-                                <input type="checkbox" name="remember" id="remember" class="form-checkbox" {{ old('remember') ? 'checked' : '' }}>
-                                <span class="ml-2">{{ __('Remember Me') }}</span>
-                            </label>
-                        </div>
-
                         <div class="flex flex-wrap items-center">
-                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                                {{ __('Login') }}
+                            <button type="submit" class="w-full bg-green-400 hover:bg-green-600 text-white font-semibold text-2xl py-6 px-4 rounded focus:outline-none focus:shadow-outline">
+                                Confirmar compra
                             </button>
-
-                            @if (Route::has('password.request'))
-                                <a class="text-sm text-blue-500 hover:text-blue-700 whitespace-no-wrap no-underline ml-auto" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <p class="w-full text-xs text-center text-gray-700 mt-8 -mb-4">
-                                    {{ __("Don't have an account?") }}
-                                    <a class="text-blue-500 hover:text-blue-700 no-underline" href="{{ route('register') }}">
-                                        {{ __('Register') }}
-                                    </a>
-                                </p>
-                            @endif
                         </div>
                     </form>
 
