f9c31f133a5b4f181c618476b5a8ebe60e1d01a0
Nelson Rodriguez <nelsonrod10@gmail.com>
2021-01-25 08:01:05 -0500

    fix: Set height to products in mobile screens

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
                         @foreach ($products as $product)
                             <div class="flex-1 h-96 px-2 py-2">
                                 <div class="rounded-md h-full md:px-6 py-0 px-2">
-                                    <div class="h-full bg-cover bg-center rounded" style="background-image: url({{asset('image/product1.jpg')}})">
-                                        <div class="" ></div>
+                                    <div class="h-1/2 md:h-full bg-cover bg-center rounded" style="background-image: url({{asset('image/product1.jpg')}})">
+                                        <div></div>
                                     </div>    
                                     
                                     <div class="flex flex-col flex-1 text-gray-800 text-xl font-light shadow-lg rounded-md py-2">
